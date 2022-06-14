123f996fd13b35d074fa4c18a23e2e051e59ee23
Rose Ilnicka <redencya@gmail.com>
2022-06-14 18:51:46 +0100

    Level Manager blank template
    Just a blank template for LevelManager logic

--- S_OperationSleuth/Levels/Level.gd
+++ S_OperationSleuth/Levels/Level.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name Level
 
 func _ready():
-	# This is used to clean up all the junk on interfaces and in logic
-	# Remove it and suddenly levels bleed into each other
-	connect("tree_exiting", GameManager, "_on_tree_exiting")
-	connect("tree_exiting", InterfaceManager, "_on_tree_exiting")
+	# Setup for LevelManager functions
+	connect("tree_exiting", LevelManager, "_cleanup")
+	connect("tree_entered", LevelManager, "_setup")
+
